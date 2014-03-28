1a834a96cbf7f5930a6d1a529a594576314199cf
wxttt <wangxiaottt@gmail.com>
2014-03-28 14:34:52 +0800

    favico修改

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('port', process.env.PORT || 7777);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(flash());
-app.use(express.favicon());
+app.use(express.favicon(path.join(__dirname, '/public/images/favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
