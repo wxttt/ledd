e48897d1e5eafd62711e0f03947bd897580f7547
wxttt <wangxiaottt@gmail.com>
2014-06-12 17:23:07 +0800

    测试hammer.js

--- public/javascripts/tomatoClock.js
+++ public/javascripts/tomatoClock.js
@@ -1,5 +1,22 @@
 /**
  * Created by wangxiaottt on 14-5-26.
  */
-var TClock = function(){};
+window.onload = function(){
+    var options = {
+        preventDefault: true,
+        swipe: true,
+        swipeMaxTouches: 1,
+        swipeMinTouches: 1,
+        swipeVelocityX: 0,
+        swipeVelocityY: 0
+    };
+    var hammertime = new Hammer(document.body, options);
+    hammertime.on("tap swipeup swipedown swipeleft swiperight", function(ev){
+        var _el = document.createElement('p');
+        var _ctn = document.getElementsByClassName('m-calendar')[0];
+        _el.innerHTML = ev.type;
+
+        _ctn.appendChild(_el);
+    });
+};
 
